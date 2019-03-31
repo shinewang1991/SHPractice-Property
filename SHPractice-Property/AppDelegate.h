dc50d59507ff53ef773b1f2c92283690f4e279ec
wangxiang <wangxiang@yixia.com>
2019-03-31 16:07:14 +0800

    Initial Commit

---
+++ SHPractice-Property/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHPractice-Property
+//
+//  Created by Shine on 2019/3/31.
+//  Copyright © 2019 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHPractice-Property/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHPractice-Property
+//
+//  Created by Shine on 2019/3/31.
+//  Copyright © 2019 shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
